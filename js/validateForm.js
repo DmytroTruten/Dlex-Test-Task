495a5175e08c2e34acfa4d383f190abc836172db
Dmytro Truten <dmitriy2503a@gmail.com>
2023-09-21 12:51:11 +0300

    fix form inputs validation, add country getter

--- js/validateForm.js
+++ js/validateForm.js
@@ -3,7 +3,15 @@ const firstNameInput = form.querySelector('input[name="first_name"]');
 const lastNameInput = form.querySelector('input[name="last_name"]');
 const emailInput = form.querySelector('input[name="email"]');
 const phoneInput = form.querySelector("input[name='phone']");
-const callbackCheckbox = form.querySelector("#callbackCheckbox");
+const callbackCheckbox = form.querySelector("#callback-checkbox");
+const errorMsg = document.querySelector("#phone-error");
+const errorMap = [
+  "Invalid number",
+  "Invalid country code",
+  "Too short",
+  "Too long",
+  "Invalid number",
+];
 
 // Function for checking email validity
 function isValidEmail(email) {
@@ -11,39 +19,84 @@ function isValidEmail(email) {
   return emailRegex.test(email);
 }
 
-// Function for checking phone validity
+// Function for checking email validity
 function isValidPhone() {
+  const inputValue = phoneInput.value.trim(); // Убираем лишние пробелы
 
-  // Checking the validity of the phone number
-  const isValid = iti.isValidNumber();
+  if (!/^\d*$/.test(inputValue)) {
+    errorMsg.innerHTML = "Please enter only numbers";
+    return false;
+  }
 
-  return isValid;
+  if (!inputValue) {
+    highlightField(phoneInput, false, errorMap[errorCode]);
+    return false;
+  }
+
+  if (iti.isValidNumber()) {
+    highlightField(phoneInput, true, "");
+    errorMsg.innerHTML = "";
+    return true;
+  } else if (inputValue.length === 1) {
+    errorMsg.innerHTML = errorMap[2];
+    return false;
+  } else {
+    const errorCode = iti.getValidationError();
+    errorMsg.innerHTML = errorMap[errorCode];
+    highlightField(phoneInput, false, errorMap[errorCode]);
+    return false;
+  }
 }
 
 // Function to highlight fields with errors
-function highlightField(input, isValid) {
+function highlightField(input, isValid, errorMessage) {
+  const errorSpan = document.getElementById(`${input.name}-error`);
   if (isValid) {
     input.style.border = "1px solid #ccc"; // Resetting the highlight
+    errorSpan.textContent = ""; // Clear the error message
   } else {
     input.style.border = "1px solid red"; // Highlight the field
+    errorSpan.textContent = errorMessage; // Set the error message
   }
 }
 
 // Event handlers for validating data input
 firstNameInput.addEventListener("input", () => {
-  highlightField(firstNameInput, firstNameInput.value.length >= 2);
+  const isValidLength = firstNameInput.value.length >= 2;
+  const isValidPattern = /^[a-zA-Z]+$/.test(firstNameInput.value);
+  const isValid = isValidLength && isValidPattern;
+  const errorMessage = isValid
+    ? ""
+    : isValidLength
+    ? "Please use only letters"
+    : "Please enter at least 2 characters.";
+
+  highlightField(firstNameInput, isValid, errorMessage);
 });
 
 lastNameInput.addEventListener("input", () => {
-  highlightField(lastNameInput, lastNameInput.value.length >= 2);
+  const isValidLength = lastNameInput.value.length >= 2;
+  const isValidPattern = /^[a-zA-Z]+$/.test(lastNameInput.value);
+  const isValid = isValidLength && isValidPattern;
+  const errorMessage = isValid
+    ? ""
+    : isValidLength
+    ? "Please use only letters"
+    : "Please enter at least 2 characters.";
+
+  highlightField(lastNameInput, isValid, errorMessage);
 });
 
 emailInput.addEventListener("input", () => {
-  highlightField(emailInput, isValidEmail(emailInput.value));
+  const isValid = isValidEmail(emailInput.value);
+  const errorMessage = isValid
+    ? ""
+    : "Please enter a valid email address (e.g., example@example.com)";
+  highlightField(emailInput, isValid, errorMessage);
 });
 
 phoneInput.addEventListener("input", () => {
-  highlightField(phoneInput, isValidPhone(phoneInput.value));
+  isValidPhone();
 });
 
 // Form submission handler
@@ -54,7 +107,6 @@ form.addEventListener("submit", (e) => {
     !isValidEmail(emailInput.value) ||
     !isValidPhone()
   ) {
-    alert("Please, set all fields correctly");
     e.preventDefault();
   } else {
     alert("You filled out the form");
